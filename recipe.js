1edc54c5367e665e0f01c32fbcdd17adb146e8c4
Renée Gyllensvaan <Rene Gyllensvaan>
2017-04-09 14:42:42 +0200

    general fixes

--- recipe.js
+++ recipe.js
@@ -1,144 +1,244 @@
 var recipe = {
-  "name": "Chokladbollar",
-  "global_img": "chokladbollar.jpg",
-  "focus_vocabulary": [
-	{"swedish": "smör", "arabic": "زبدة"},
-    {"swedish": "strösocker", "arabic": "سكر"},
-    {"swedish": "vaniljsocker", "arabic": "الفانيليا"},
-    {"swedish": "snabbkaffepulver", "arabic": "قهوة"},
-    {"swedish": "vatten", "arabic": "ماء"},
-    {"swedish": "kakao", "arabic": "كاكاو"},
-    {"swedish": "havregryn", "arabic": "دقيق الشوفان"},
-    {"swedish": "kokosflingor", "arabic": "جوز الهند"}
-  ],
-  "steps":[
-  {
-    "number": "01",
-    "text": "Mät upp smör, strösocker och vaniljsocker i en bunke. Låt stå framme i rumstemperatur tills ______ blivit rumsvarmt. Skär gärna smöret i mindre bitar så går det fort. Vispa till en jämn smet med elvisp.",
-    "translation_google": ["قياس,Mät", "الزبدة,upp smör", "حبيبات,strösocker", "السكر,vaniljsocker", "و,och", "في وعاء,i en bunke", "اسمحوا الوقوف,Låt stå","أمام,framme i","درجة حرارة الغرفة حتى,rumstemperatur tills","الزبدة,smöret","تصبح ساخنة,blivit rumsvarmt","قطع,Skär","الزبدة,smöret","بكل سرور,gärna","إلى قطع صغيرة,i mindre bitar","لذلك,så det","يذهب بسرعة,går fort","ضربت حتى,Vispa till","عجينة ناعمة,en jämn smet","الكهربائية,med elvisp"],
-    "translation_human": ["أمزج,Mät","زبدة,smör","سكر,strösocker","و,och","سكر,vaniljsocker","في ,i en","الوعاء,bunke","إقطع,Skär","الزبدة,smöret","صغيرة,mindre","قطع,bitar","ل,så","تصبح,går","سريعة,fort","أُخفق,Vispa","بتساوي,jämn","المزيج,smet","الخلاط,elvisp"],
-    "lemmatized_text": "mäta upp smör, strösocker och vaniljsocker i en bunke. låta stå framme i rumstemperatur tills ______ bli rumsvarm. skära gärna smör i liten bit så gå den fort. vispa till en jämn smet med elvisp.",
-    "solution": "smöret",
-    "figures": [
+   "name":"Chokladbollar",
+   "global_img":"chokladbollar.jpg",
+   "focus_vocabulary":[
+		{"swedish": "smör", "arabic": "زبدة"},
+		{"swedish": "strösocker", "arabic": "سكر"},
+		{"swedish": "vaniljsocker", "arabic": "الفانيليا"},
+		{"swedish": "snabbkaffepulver", "arabic": "قهوة"},
+		{"swedish": "vatten", "arabic": "ماء"},
+		{"swedish": "kakao", "arabic": "كاكاو"},
+		{"swedish": "havregryn", "arabic": "دقيق الشوفان"},
+		{"swedish": "kokosflingor", "arabic": "جوز الهند"}
+   ],
+   "steps":[
       {
-        "img": "smoer-stroesocker-vaniljsocker.jpg",
-        "caption": "Smör, strösocker, vaniljsocker."
+         "number":"01",
+         "text":"Mät upp smör, strösocker och vaniljsocker i en bunke. Låt stå framme i rumstemperatur tills smöret blivit rumsvarmt. Skär gärna smöret i mindre bitar så går det fort. Vispa till en jämn smet med elvisp.",
+         "translation_google":[
+            "قياس,Mät",
+            "الزبدة,upp smör",
+            "حبيبات,strösocker",
+            "السكر,vaniljsocker",
+            "و,och",
+            "في وعاء,i en bunke",
+            "اسمحوا الوقوف,Låt stå",
+            "أمام,framme i",
+            "درجة حرارة الغرفة حتى,rumstemperatur tills",
+            "الزبدة,smöret",
+            "تصبح ساخنة,blivit rumsvarmt",
+            "قطع,Skär",
+            "الزبدة,smöret",
+            "بكل سرور,gärna",
+            "إلى قطع صغيرة,i mindre bitar",
+            "لذلك,så det",
+            "يذهب بسرعة,går fort",
+            "ضربت حتى,Vispa till",
+            "عجينة ناعمة,en jämn smet",
+            "الكهربائية,med elvisp"
+         ],
+         "translation_human":[
+            "أمزج,Mät",
+            ",upp,",
+            "زبدة,smör,",
+            "سكر,strösocker",
+            "و,och",
+            "سكر,vaniljsocker",
+            "في ,i en",
+            "الوعاء,bunke.",
+            ",Låt",
+            "كان,stå",
+            ",framme",
+            ",i",
+            ",rumstemperatur",
+            ",tills",
+            "الزبدة,______",
+            ",blivit",
+            "دافئ,rumsvarmt.",
+            "إقطع,Skär",
+            ",gärna",
+            "الزبدة,smöret",
+            ",i",
+            "صغيرة,mindre",
+            "قطع,bitar",
+            "ل,så",
+            "تصبح,går",
+            ",det",
+            "سريعة,fort",
+            "أُخفق,______",
+            ",till en",
+            "بتساوي,jämn",
+            "المزيج,smet",
+            ",med",
+            "الخلاط,elvisp."
+         ],
+         "translation_human_full":[
+            "أمزج,Mät",
+            ",upp,",
+            "زبدة,smör",
+            "سكر,strösocker",
+            "و,och",
+            "سكر,vaniljsocker",
+            "في ,i en",
+            "الوعاء,bunke.",
+            ",Låt",
+            "كان,stå",
+            ",framme",
+            ",i",
+            ",rumstemperatur",
+            ",tills",
+            "الزبدة,smöret",
+            ",blivit",
+            "دافئ,rumsvarmt.",
+            "إقطع,Skär",
+            ",gärna",
+            "الزبدة,smöret",
+            ",i",
+            "صغيرة,mindre",
+            "قطع,bitar",
+            "ل,så",
+            "تصبح,går",
+            ",det",
+            "سريعة,fort",
+            "أُخفق,vispa",
+            ",till en",
+            "بتساوي,jämn",
+            "المزيج,smet",
+            ",med",
+            "الخلاط,elvisp."
+         ],
+         "lemmatized_text":"mäta upp smör, strösocker och vaniljsocker i en bunke. låta stå framme i rumstemperatur tills ______ bli rumsvarm. skära gärna smör i liten bit så gå den fort. vispa till en jämn smet med elvisp.",
+         "solutions":[
+            "smöret",
+            "vispa"
+         ],
+         "suggestions":[
+            "vispa",
+            "smöret",
+            "rulla",
+            "kylen"
+         ],
+         "figures":[
+            {
+               "img":"smoer-stroesocker-vaniljsocker.jpg",
+               "caption":"Smör, strösocker, vaniljsocker."
+            },
+            {
+               "img":"smoer-socker-vispat.jpg",
+               "caption":"Smör och socker vispat."
+            }
+         ],
+         "ingredients":[
+            {
+               "quantity":"150 g",
+               "item":"smör"
+            },
+            {
+               "quantity":"1.5 dl",
+               "item":"strösocker"
+            },
+            {
+               "quantity":"1 msk",
+               "item":"vaniljsocker"
+            }
+         ]
       },
       {
-        "img": "smoer-socker-vispat.jpg",
-        "caption": "Smör och socker vispat."
-      }
-    ],
-    "ingredients": [
-      {
-        "quantity": "150 g",
-        "item": "smör"
+         "number":"02",
+         "text":"Om du väljer alternativet kaffe med snabbkaffepulver - blanda då snabbkaffepulver med vatten. (Det går också bra att använda bryggt kaffe eller något av de andra alternativen vanligt vatten eller apelsinjuice beroende på vilken smak man vill ha på sina chokladbollar, se ingredienslistan.)",
+         "lemmatized_text":"om du välja alternative kaffe med snabbkaffepulver - blanda då snabbkaffepulver med ______. (det gå också bra att använda brygga kaffe eller någon av de annan alternativ vanlig vatten eller apelsinjuice beroende på vilken smak man vilja ha på sin chokladboll, se ingredienslistan.)",
+         "solution":"vatten",
+         "figures":[
+            {
+               "img":"vatten-snabbkaffepulver.jpg",
+               "caption":"Vatten och snabbkaffepulver."
+            }
+         ],
+         "ingredients":[
+            {
+               "quantity":"2-3 tsk",
+               "item":"snabbkaffepulver"
+            },
+            {
+               "quantity":"2-3 msk",
+               "item":"vatten"
+            }
+         ]
       },
       {
-        "quantity": "1.5 dl",
-        "item": "strösocker"
+         "number":"03",
+         "text":"Tillsätt kaffe (eller vatten eller apelsinjuice), kakao, havregryn och kokosflingor och rör till en jämn smet med elvisp. Kör gärna lite extra, gärna ca 5 minuter med elvispen så blir smeten ännu godare.",
+         "solution":"havregryn",
+         "figures":[
+            {
+               "img":"ingredienser-chokladbollssmet.jpg",
+               "caption":"Ingredienser till chokladbollssmeten tillsatta."
+            },
+            {
+               "img":"chokladbollssmet.jpg",
+               "caption":"Chokladbollssmeten."
+            }
+         ],
+         "ingredients":[
+            {
+               "quantity":"2-3 tsk",
+               "item":"snabbkaffepulver"
+            },
+            {
+               "quantity":"2-3 msk",
+               "item":"vatten"
+            },
+            {
+               "quantity":"4 msk",
+               "item":"kakao"
+            },
+            {
+               "quantity":"3.5 dl",
+               "item":"havregryn"
+            },
+            {
+               "quantity":"1 dl",
+               "item":"kokosflingor"
+            }
+         ]
       },
       {
-        "quantity": "1 msk",
-        "item": "vaniljsocker"
-      }
-    ]
-  },
-  {
-    "number": "02",
-    "text": "Om du väljer alternativet kaffe med snabbkaffepulver - blanda då snabbkaffepulver med ______. (Det går också bra att använda bryggt kaffe eller något av de andra alternativen vanligt vatten eller apelsinjuice beroende på vilken smak man vill ha på sina chokladbollar, se ingredienslistan.)",
-    "lemmatized_text": "om du välja alternative kaffe med snabbkaffepulver - blanda då snabbkaffepulver med ______. (det gå också bra att använda brygga kaffe eller någon av de annan alternativ vanlig vatten eller apelsinjuice beroende på vilken smak man vilja ha på sin chokladboll, se ingredienslistan.)",
-    "solution": "vatten",
-    "figures": [
-      {
-        "img": "vatten-snabbkaffepulver.jpg",
-        "caption": "Vatten och snabbkaffepulver."
-      }
-    ],
-    "ingredients": [
-      {
-        "quantity": "2-3 tsk",
-        "item": "snabbkaffepulver"
+         "number":"04",
+         "text":"Om smeten är väldigt lös och kletig kan man ställa in den i kylen eller frysen en stund innan man rullar bollar. Häll upp kokosflingor eller pärlsocker i en djup tallrik. Rulla bollar i valfri storlek av smeten och rulla bollarna i kokosflingorna eller pärlsockret. Lägg chokladbollarna i en burk och förvara i kylskåp. De går också att frysa in.",
+         "solution":"kokosflingorna",
+         "figures":[
+            {
+               "img":"chokladboll-kokosflingor.jpg",
+               "caption":"Chokladboll och kokosflingor."
+            },
+            {
+               "img":"chokladboll-rullad-i-kokosflingor.jpg",
+               "caption":"Chokladboll rullad i kokosflingor."
+            }
+         ],
+         "ingredients":[
+            {
+               "quantity":"0.75 dl",
+               "item":"kokosflingor"
+            }
+         ]
       },
       {
-        "quantity": "2-3 msk",
-        "item": "vatten"
+         "number":"05",
+         "text":"Servera chokladbollarna kylskåpskalla, då är de godast tycker jag. Man kan annars låta dem stå framme i rumstemperatur en stund så blir de mjukare men även kladdigare.",
+         "solution":"",
+         "figures":[
+            {
+               "img":"serverade-chokladbollar.jpg",
+               "caption":"Serverade chokladbollar."
+            }
+         ],
+         "ingredients":[
+            {
+               "quantity":"",
+               "item":""
+            }
+         ]
       }
-    ] 
-  },
-  {
-    "number": "03",
-    "text": "Tillsätt kaffe (eller vatten eller apelsinjuice), kakao, ______ och kokosflingor och rör till en jämn smet med elvisp. Kör gärna lite extra, gärna ca 5 minuter med elvispen så blir smeten ännu godare.",
-    "solution": "havregryn",
-    "figures": [
-      {
-        "img": "ingredienser-chokladbollssmet.jpg",
-        "caption": "Ingredienser till chokladbollssmeten tillsatta."
-      },
-      {
-        "img": "chokladbollssmet.jpg",
-        "caption": "Chokladbollssmeten."
-      }
-    ],
-    "ingredients": [
-      {
-        "quantity": "2-3 tsk",
-        "item": "snabbkaffepulver"
-      },
-      {
-        "quantity": "2-3 msk",
-        "item": "vatten"
-      },
-      {
-        "quantity": "4 msk",
-        "item": "kakao"
-      },
-      {
-        "quantity": "3.5 dl",
-        "item": "havregryn"
-      },
-      {
-        "quantity": "1 dl",
-        "item": "kokosflingor"
-      }
-    ]
-  },
-  {
-    "number": "04",
-    "text": "Om smeten är väldigt lös och kletig kan man ställa in den i kylen eller frysen en stund innan man rullar bollar. Häll upp kokosflingor eller pärlsocker i en djup tallrik. Rulla bollar i valfri storlek av smeten och rulla bollarna i ______ eller pärlsockret. Lägg chokladbollarna i en burk och förvara i kylskåp. De går också att frysa in.",
-    "solution": "kokosflingorna",
-    "figures": [
-      {
-        "img": "chokladboll-kokosflingor.jpg",
-        "caption": "Chokladboll och kokosflingor."
-      },
-      {
-        "img": "chokladboll-rullad-i-kokosflingor.jpg",
-        "caption": "Chokladboll rullad i kokosflingor."
-      }
-    ],
-    "ingredients": [
-      {
-        "quantity": "0.75 dl",
-        "item": "kokosflingor"
-      }
-    ]
-  },
-  {
-    "number": "05",
-    "text": "Servera chokladbollarna kylskåpskalla, då är de godast tycker jag. Man kan annars låta dem stå framme i rumstemperatur en stund så blir de mjukare men även kladdigare.",
-    "solution": "",
-    "figures": [
-      {
-        "img": "serverade-chokladbollar.jpg",
-        "caption": "Serverade chokladbollar."
-      }
-    ],
-    "ingredients": [
-      {
-        "quantity": "",
-        "item": ""
-      }
-    ]
-  }]
+   ]
 }
